ff3f5d9f5cf8a057bd199e998ca6463f9280a312
Chau Lucky Nguyen <lkn007@ucsd.edu>
2016-04-23 15:12:48 -0700

    working on gender graph

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,33 @@
 (function(d3) {
   "use strict";
 
-  var data = [
-    { name: "Lolita's", rating: 7.5 },
-    { name: "Lucha Libre", rating: 8 },
-    { name: "Puesto", rating: 9.5 },
-    { name: "Rubio's", rating: 4 },
-    { name: "Taco Bell", rating: 3 },
-    { name: "Taco Stand", rating: 8.5 },
-    { name: "Taco's, El Gordo", rating: 9 },
-    { name: "Oscar's", rating: 9 },
-    { name: "Rigoberto's", rating: 6 },
-    { name: "Galaxy Taco", rating: 6.5 },
-  ];
+ // var data = [
+ //   { name: "Lolita's", rating: 7.5 },
+  //  { name: "Lucha Libre", rating: 8 },
+  //  { name: "Puesto", rating: 9.5 },
+ //   { name: "Rubio's", rating: 4 },
+ //   { name: "Taco Bell", rating: 3 },
+ //   { name: "Taco Stand", rating: 8.5 },
+ //   { name: "Taco's, El Gordo", rating: 9 },
+  //  { name: "Oscar's", rating: 9 },
+  //  { name: "Rigoberto's", rating: 6 },
+  //  { name: "Galaxy Taco", rating: 6.5 },
+ // ];
+
+
+  // Grab the delphi data from the server
+  d3.json("/delphidata", function(err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Data", data);
+  
+
 
   // Defining the margins and chart size
   // See margin conventions for more information
-  var margin = {top: 20, right: 10, bottom: 100, left: 40},
+  var margin = {top: 20, right: 10, bottom: 100, left: 60},
       width = 960 - margin.right - margin.left,
       height = 500 - margin.top - margin.bottom;
 
@@ -38,9 +49,9 @@
                 .attr("transform", "translate(" +  margin.left + "," + margin.right + ")");
 
   // Render the chart
-  xScale.domain(data.map(function (d){ return d.name; }));
+  xScale.domain(data.map(function (d){ return d.gender; }));
   // TODO: Fix the yScale domain to scale with any ratings range
-  yScale.domain([ d3.max(data, function(d){return d.rating}), 0]);
+  yScale.domain([ d3.max(data, function(d){return d.number_of_respondents}), 0]);
 
   // Note all these values are hard coded numbers
   // TODO:
@@ -48,12 +59,12 @@
   // 2. Update the x, y, width, and height attributes to appropriate reflect this
  chart
     .selectAll(".bar")
-    .data(data.map(function (d){ return d.rating; }))
+    .data(data.map(function (d){ return d.number_of_respondents; }))
     .enter().append("rect")
     .attr("class", "bar")
-    .attr("x", function(d, i) { return d + i * width / data.length; })
-    .attr("width", xScale.rangeBand() - 20)
-    .attr("y", function(d){ return height - d * 40;} )
+    .attr("x", function(d, i) { return d + i * width/ data.length; })
+    .attr("width", xScale.rangeBand()- 20)
+    .attr("y", function(d){ return height - d * 40 ;} )
     .attr("height", function(d) { return d * 40; });
 
   // Orient the x and y axis
@@ -63,28 +74,17 @@
   // TODO: Append X axis
   chart
     .append("g")
-    .attr("transform", "translate(0," + height+ ")")
+    .attr("transform", "translate(0," + height + ")")
     .call(xAxis)
     .selectAll("text")
     .attr("y", 0)
     .attr("x", -60)
     .attr("transform", "rotate(-60)");
 
-
-
   // TODO: Append Y axis
   chart
     .append("g").call(yAxis);
 
-
-  // ASSIGNMENT PART 1B
-  // Grab the delphi data from the server
-  d3.json("/delphidata", function(err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Data", data);
-  });
+});//end of delphi
 
 })(d3);
