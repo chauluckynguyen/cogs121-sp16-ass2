a0b491fa2377656486788a81b15a9b151d45e8fe
Steven Lim <stevenlim890@gmail.com>
2016-04-23 13:25:04 -0700

    got query data

--- server.js
+++ server.js
@@ -34,18 +34,27 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-app.get('/delphidata', function (req, res) {
-  // TODO
-  // Connect to the DELPHI Database and return the proper information
-  // that will be displayed on the D3 visualization
-  // Table: Smoking Prevalance in Adults
-  // Task: In the year 2003, retrieve the total number of respondents
-  // for each gender. 
-  // Display that data using D3 with gender on the x-axis and 
-  // total respondents on the y-axis.
-  return { delphidata: "No data present." }
-});
+var query = 'SELECT cdph_smoking_prevalence_in_adults_1984_2013.gender, cdph_smoking_prevalence_in_adults_1984_2013.number_of_respondents FROM cogs121_16_raw.cdph_smoking_prevalence_in_adults_1984_2013 WHERE cdph_smoking_prevalence_in_adults_1984_2013.year = \'2013\''; 
 
+app.get('/delphidata', function (req, res) {
+
+    var pg = require('pg');
+
+    pg.connect(conString, function(err, client, done) {
+        if(err) {
+            return console.error('error fetching client from pool', err);
+        }
+
+        client.query(query, function(err, result) {
+            done();
+
+            if(err) {
+              return console.error('error running query', err);
+            }
+            res.json(result.rows);
+        });
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
